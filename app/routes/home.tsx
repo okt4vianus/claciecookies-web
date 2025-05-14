49f84b7deb579aec2ae558b1f2d2022828f0c7a8
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-05-14 23:41:08 +0800

    feat: image zoom

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,6 +1,5 @@
 import type { Route } from "./+types/home";
 import type { ManyProductsResponse } from "~/modules/product/schema";
-import { motion } from "framer-motion";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -25,7 +24,6 @@ export async function loader({}: Route.LoaderArgs) {
     return products;
   } catch (error) {
     console.error("Error fetching products:", error);
-    // throw new Response("Failed to load products", { status: 500 });
     return [];
   }
 }
@@ -34,40 +32,47 @@ export default function Home({ loaderData }: Route.ComponentProps) {
   const products = loaderData as ManyProductsResponse;
 
   return (
-    <div className="p-4 max-w-7xl mx-auto bg-[#5C4033] text-white min-h-screen">
+    <div
+      className="p-4 max-w-7xl mx-auto bg-yellow-950
+ text-white min-h-screen"
+    >
       <h1 className="text-3xl font-bold mb-6 text-center">Home</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 items-stretch">
-        {products.map((product) => (
-          <div
-            key={product.id}
-            className="bg-[#5C4033] text-white rounded-2xl overflow-hidden flex flex-col h-120 shadow-md transition-shadow border border-white"
-          >
-            {product.images?.[0].url && (
-              <motion.img
-                src={product.images[0].url}
-                alt={product.images[0].name || "Product image"}
-                className="w-full h-70 object-cover"
-                whileHover={{ scale: 1.05 }}
-                transition={{ type: "spring", stiffness: 200, damping: 20 }}
-              />
-            )}
+        {products.map((product) => {
+          const image = product.images?.[0];
 
-            <div className="p-4 flex flex-col flex-grow">
-              <p className="font-medium mb-3">
-                Rp {product.price.toLocaleString("id-ID")}
-              </p>
-              <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
-              {/* <p className="text-sm mb-1 flex-grow">{product.description}</p> */}
-              {/* <p className="font-medium mb-4">Stock: {product.stockQuantity}</p> */}
+          if (!image) return null; // Skip rendering if no image is available
 
-              <div className="mt-auto flex justify-center">
-                <button className="w-1/2 bg-[#FFF5E1] text-[#5C4033] border border-[#5C4033] py-2 px-4 rounded-full hover:bg-red-600 hover:text-white transition-colors">
-                  Buy now
-                </button>
+          return (
+            <div
+              key={product.id}
+              className="bg-amber-950 text-white rounded-2xl overflow-hidden flex flex-col h-120 shadow-md transition-shadow border border-white"
+            >
+              <picture className="rounded-lg block overflow-hidden">
+                <img
+                  className="hover:scale-110 transition duration-500 ease-in-out h-70 w-full object-cover"
+                  src={image.url}
+                  alt={image.name || "Product image"}
+                />
+              </picture>
+
+              <div className="p-4 flex flex-col flex-grow">
+                <p className="font-medium mb-3">
+                  Rp {product.price.toLocaleString("id-ID")}
+                </p>
+                <h2 className="text-xl font-semibold mb-2">{product.name}</h2>
+                {/* <p className="text-sm mb-1 flex-grow">{product.description}</p> */}
+                {/* <p className="font-medium mb-4">Stock: {product.stockQuantity}</p> */}
+
+                <div className="mt-auto flex justify-center">
+                  <button className="w-1/2 bg-[#FFF5E1] text-[#5C4033] border border-[#5C4033] py-2 px-4 rounded-full hover:bg-red-600 hover:text-white transition-colors">
+                    Buy now
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        ))}
+          );
+        })}
       </div>
     </div>
   );
