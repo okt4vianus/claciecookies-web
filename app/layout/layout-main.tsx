8d5db433d5d8f6d67fa5e1a6fab3a98db0286adc
M Haidar Hanif <git@mhaidarhanif.com>
2025-05-31 15:11:37 +0700

    feat: put route search, layout form search to action /search

--- app/layout/layout-main.tsx
+++ app/layout/layout-main.tsx
@@ -7,7 +7,7 @@ export default function MainLayoutRoute() {
   return (
     <div>
       <nav className="bg-background text-foreground border-b border-border shadow-sm">
-        <div className="font-[Sunday] max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
+        <div className="max-w-7xl mx-auto px-4 py-3 flex justify-between items-center">
           <Link
             to="/"
             className="text-2xl font-semibold tracking-tight text-primary hover:text-accent"
@@ -16,23 +16,19 @@ export default function MainLayoutRoute() {
           </Link>
 
           <Form
-            // action="/search"
+            action="/search"
             method="get"
-            className="relative w-full max-w-md"
+            className="flex gap-2 w-full max-w-md"
           >
             <Input
               name="q"
               type="search"
               placeholder="Search cookies..."
-              className="rounded-full bg-white/90 backdrop-blur-md placeholder:text-muted-foreground text-sm px-5 py-2 pr-10 shadow-inner focus:ring-2 focus:ring-primary transition"
+              className="rounded-full bg-white/90 backdrop-blur-md placeholder:text-muted-foreground text-sm px-5 py-2 shadow-inner focus:ring-2 focus:ring-primary transition"
             />
-            <Button
-              type="submit"
-              size="icon"
-              variant="secondary"
-              className="absolute right-1.5 top-1/2 -translate-y-1/2"
-            >
+            <Button type="submit" variant="secondary" className="rounded-full">
               <Search className="w-4 h-4" />
+              <span className="text-sm">Search</span>
             </Button>
           </Form>
 
