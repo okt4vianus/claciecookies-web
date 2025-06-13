d509b2e6af92ff0de620aaee7b3d305f67474af8
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-06-13 17:08:07 +0800

    feat: install alert componen & create alert-error componen

---
+++ app/components/common/alert-error.tsx
@@ -0,0 +1,41 @@
+import { AlertCircleIcon } from "lucide-react";
+import { Alert, AlertDescription, AlertTitle } from "../ui/alert";
+
+export function AlertError({
+  errors,
+}: {
+  title?: string;
+  errors: string[] | undefined;
+}) {
+  return (
+    <Alert variant="destructive">
+      <AlertCircleIcon />
+      <AlertDescription>
+        <ul className="text-sm">
+          {errors?.map((errorText) => (
+            <li>{errorText}</li>
+          ))}
+        </ul>
+      </AlertDescription>
+    </Alert>
+  );
+}
+
+export function AlertErrorSimple({
+  errors,
+}: {
+  title?: string;
+  errors: string[] | undefined;
+}) {
+  return (
+    <Alert variant="destructive" className="px-2 py-1">
+      <AlertDescription>
+        <ul className="text-xs">
+          {errors?.map((errorText) => (
+            <li>{errorText}</li>
+          ))}
+        </ul>
+      </AlertDescription>
+    </Alert>
+  );
+}
