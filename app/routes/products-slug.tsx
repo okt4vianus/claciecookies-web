16901c2eb7b7e0f2b74288c2207c7469651f83ef
Oktavianus R. Tasak <okt4vianus@gmail.com>
2025-06-19 11:53:14 +0800

    feat: update layout and login components for improved styling and authentication handling

--- app/routes/products-slug.tsx
+++ app/routes/products-slug.tsx
@@ -8,8 +8,6 @@ import { Label } from "~/components/ui/label";
 import { apiClient } from "~/lib/api-client";
 import { getSession } from "~/sessions.server";
 import type { Route } from "./+types/products-slug";
-import { Form, redirect, useNavigation } from "react-router";
-import { getSession } from "~/sessions.server";
 
 export function meta({ data }: Route.MetaArgs) {
   if (!data || !data.product) {
@@ -46,7 +44,12 @@ export async function loader({ params, request }: Route.LoaderArgs) {
 export async function action({ request }: Route.ActionArgs) {
   const session = await getSession(request.headers.get("Cookie"));
   const token = session.get("token");
-  if (!token) return new Response("Unauthorized", { status: 400 });
+
+  // if (!token) return new Response("Unauthorized", { status: 400 });
+  if (!token) {
+    // Redirect to login page if not authorized
+    return redirect(href("/login"));
+  }
 
   const formData = await request.formData();
   const productId = formData.get("productId");
