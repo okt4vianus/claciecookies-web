7184bd818a430a59c2bc085458402474faf3d9f2
M Haidar Hanif <git@mhaidarhanif.com>
2025-07-29 16:07:15 +0700

    refactor: streamline Google authentication actions and improve session handling

--- app/routes/auth/callback/google.ts
+++ app/routes/auth/callback/google.ts
@@ -1,12 +1,30 @@
-import { redirect } from "react-router";
+import { href, redirect } from "react-router";
+import { commitAppSession, getAppSession } from "@/app-session.server";
+import { betterAuthApiClient } from "@/lib/api-client";
 import type { Route } from "./+types/google";
 
 /**
  * After Google sign in success,
  * it will GET /auth/callback/google
  */
-export async function loader(_: Route.LoaderArgs) {
+export async function loader({ request }: Route.LoaderArgs) {
   console.log("Google sign in successful");
 
-  return redirect("/");
+  const session = await getAppSession(request.headers.get("Cookie"));
+
+  const { data, error } = await betterAuthApiClient.GET("/get-session");
+
+  console.log({ data });
+
+  if (!data || !data.user.id || error) {
+    return redirect(href("/login"));
+  }
+
+  session.set("userId", data.user.id);
+  session.set("user", data.user);
+  session.set("toastMessage", `Welcome back, ${data.user.name}`);
+
+  return redirect(href("/"), {
+    headers: { "Set-Cookie": await commitAppSession(session) },
+  });
 }
