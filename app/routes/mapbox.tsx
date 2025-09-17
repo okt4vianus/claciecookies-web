a55310e736ada76415924a94cec05fa6b1b38fe0
M Haidar Hanif <git@mhaidarhanif.com>
2025-09-17 16:00:38 +0700

    feat: mapbox view component, env VITE_MAPBOX_ACCESS_TOKEN

--- app/routes/mapbox.tsx
+++ app/routes/mapbox.tsx
@@ -1,7 +1,11 @@
+import { MapboxView } from "@/modules/maps/mapbox-view";
+
 export default function MapboxRoute() {
   return (
     <div>
       <h1>Mapbox</h1>
+
+      <MapboxView />
     </div>
   );
 }
